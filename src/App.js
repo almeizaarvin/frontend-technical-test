c504e9574a886bb29cd86aed30bfbd1bbf7b72b9
almeizaarvin <13519066@std.stei.itb.ac.id>
2022-04-02 12:20:25 +0700

    Modify response data

--- src/App.js
+++ src/App.js
@@ -1,59 +1,85 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  const response = {
-    message: "Success to get data",
-    data: [
-      {
-        id: "1",
-        first_name: "John",
-        last_name: "Doe",
-        skills: {
-          javascript: "advanced",
-          python: "basic",
-          golang: "expert",
-          php: "expert",
-        },
-      },
-      {
-        id: "2",
-        first_name: "Will",
-        last_name: "Smith",
-        skills: {
-          javascript: "expert",
-          python: "basic",
-          golang: "advanced",
-          php: "advanced",
-        },
-      },
-      {
-        id: "3",
-        first_name: "Jaden",
-        last_name: "Smith",
-        skills: {
-          javascript: "expert",
-          python: "expert",
-          golang: "expert",
-          php: "expert",
-        },
-      },
-      {
-        id: "4",
-        first_name: "El",
-        last_name: "Professor",
-        skills: {
-          javascript: "advanced",
-          python: "basic",
-          golang: "intermediate",
-          php: "intermediate",
-        },
-      },
-    ],
-  };
-  
-  return (
-    <p>Helloworld</p>
-  );
+    const [persons, setPersons] = useState([]);
+
+    const response = {
+        message: "Success to get data",
+        data: [
+            {
+                id: "1",
+                first_name: "John",
+                last_name: "Doe",
+                skills: {
+                    javascript: "advanced",
+                    python: "basic",
+                    golang: "expert",
+                    php: "expert",
+                },
+            },
+            {
+                id: "2",
+                first_name: "Will",
+                last_name: "Smith",
+                skills: {
+                    javascript: "expert",
+                    python: "basic",
+                    golang: "advanced",
+                    php: "advanced",
+                },
+            },
+            {
+                id: "3",
+                first_name: "Jaden",
+                last_name: "Smith",
+                skills: {
+                    javascript: "expert",
+                    python: "expert",
+                    golang: "expert",
+                    php: "expert",
+                },
+            },
+            {
+                id: "4",
+                first_name: "El",
+                last_name: "Professor",
+                skills: {
+                    javascript: "advanced",
+                    python: "basic",
+                    golang: "intermediate",
+                    php: "intermediate",
+                },
+            },
+        ],
+    };
+
+    function personSkills(skills) {
+        var personSkills = [];
+        for (let j in skills) {
+            if (skills[j] === "expert") {
+                personSkills.push(j);
+            }
+        }
+        return personSkills;
+    }
+
+    var personsData = [];
+    for (let i in response.data) {
+        var p = {
+            id: response.data[i].id,
+            full_name:
+                response.data[i].first_name + " " + response.data[i].last_name,
+            expert_skills: personSkills(response.data[i].skills),
+        };
+        personsData.push(p);
+    }
+
+    setPersons(personsData);
+
+    console.log(persons);
+
+    return <p>Helloworld</p>;
 }
 
 export default App;
